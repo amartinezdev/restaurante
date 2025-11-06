f6c1c6263390974229f9102a64fff29ebd405ccb
amartinezdev <alvarowkz@gmail.com>
2025-11-06 08:59:39 +0100

    Navbar de cliente terminado

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include("components/conexion.php");
   <title>El Quinto Pino</title>
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
   <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" />
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" /> -->
   <link rel="stylesheet" href="styles.css" />
 </head>
 
@@ -82,7 +82,11 @@ include("components/conexion.php");
             <label for="pw">Contraseña</label>
           </div>
           <div class="d-grid">
-            <button type="submit" class="btn btn-light fw-bold lead"><i class="bi bi-box-arrow-in-right"></i> Entrar</button>
+            <button type="submit" class="btn btn-light fw-bold lead">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z" />
+                <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+              </svg> Entrar</button>
           </div>
 
           <div class="text-center mt-4 text-warning">
