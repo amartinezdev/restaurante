45a48681f13d35974dec0c2ad833aaf49b40d063
alvarowkz <alvarowkz@gmail.com>
2025-11-06 01:29:43 +0100

    el señor de la noche

--- cliente/perfil.php
+++ cliente/perfil.php
@@ -21,7 +21,7 @@ $active = "perfil";
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>El Quinto Pino - Cliente</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" /> -->
     <link rel="stylesheet" href="../styles.css" />
 </head>
 
