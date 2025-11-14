b4ba28ac8345be35a872cbbcf58c90144fb6c7ff
alvarowkz <alvarowkz@gmail.com>
2025-11-14 16:32:34 +0100

    mejora en la bbdd

--- bd/restaurante_06.sql
+++ bd/restaurante_06.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-11-2025 a las 15:52:35
+-- Tiempo de generación: 14-11-2025 a las 16:32:07
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
